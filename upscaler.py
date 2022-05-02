e710ad66466c34de4cf1bba96661f00502c7d8de
Nikolas-Dmitrovic <nikolasdmitrovic687@gmail.com>
2022-05-01 22:26:05 -0400

    Update upscaler.py

--- upscaler.py
+++ upscaler.py
@@ -43,6 +43,29 @@ class upscaler():
             }
         return supported_file_types
 
+    def subprocess_args(self, include_stdout=True):
+        # See https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess
+        # for reference and comments.
+
+        kwargs = {
+            'stdin': subprocess.PIPE,
+            'stderr': subprocess.PIPE,
+            'startupinfo': None,
+            'env': os.environ,
+        }
+
+        if hasattr(subprocess, 'STARTUPINFO'):
+            kwargs['startupinfo'] = subprocess.STARTUPINFO()
+            kwargs['startupinfo'].dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            kwargs['startupinfo'].wShowWindow = subprocess.SW_HIDE
+
+        if include_stdout:
+            kwargs['stdout'] = subprocess.PIPE
+        else:
+            kwargs['stdout'] = subprocess.DEVNULL
+
+        return kwargs
+
     # TODO add **args and parse for flags
     # or just have each flag as a kwarg
     def run(self, file_location, save_location):
@@ -72,8 +95,57 @@ class wifu2x(upscaler):
     def __init__(self, Binary_location=None) -> None:
         super().__init__(Binary_location)
     
-    def run(self):
-        pass
+
+    #TODO clean up inputs to get rid of uneeded flags
+    def run(self, 
+        file_location, 
+        save_location,
+        noise_level = 0, # defult value for algorithm 
+        scale = 2, # defult value for algorithm 
+        tile_size = None,
+        model_path = "models-cunet",
+        gpu_id = None,
+        load_proc_save = None,
+        tta_mode = False,
+        output_format = "png"):
+
+        if file_location == None: return 1
+        if self.file_type(file_location) not in self.supported_file_types(): return 2
+
+
+        command = []
+        # TODO add checks to see if file paths are valid
+        command += (self.binary_location, "-n", self.file_input, "-o",self.save_location)
+
+        if noise_level > 3: return # throw error
+        if noise_level > 0: command+= ("-n", noise_level)
+        
+        algorithms_scales = [1,2,4,8,16,32]
+
+        if scale != 1 and scale in algorithms_scales: command+=("-s", scale)
+        if scale not in algorithms_scales: return # throw error
+
+        if tile_size != None: return #throw error if it is anything other than none
+
+        algorithms_models = ["models-cunet", "models-upconv_7_anime_style_art_rgb", "models-upconv_7_photo"]
+
+        if model_path != "algorithms_models" and model_path in algorithms_models: command += ("-m", model_path)
+        if model_path not in algorithms_models: return # call error
+
+        if gpu_id != None: return #throw error if it is anything other than none
+        if load_proc_save != None: return #throw error if it is anything other than none
+        if tta_mode != None: return #throw error if it is anything other than none
+
+        output_formats = ["jpg", "png", "webp"]
+
+        if output_format != "png" and output_format in output_formats: command += ("-f", format)
+
+        try:
+            process = subprocess.Popen(command, **self.subprocess_args())
+        except:
+            #TODO add error handling
+            pass
+
 
 class esgran(upscaler):
     def __init__(self, Binary_location=None) -> None:
